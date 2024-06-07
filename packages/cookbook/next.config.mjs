f1f9ed3fabb5bb872b263d60714eab76230d98ab
Ján Korček <korcekdevelopment@gmail.com>
2024-06-07 20:25:01 +0200

    chore: update packages

--- packages/cookbook/next.config.mjs
+++ packages/cookbook/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@cs/ui', '@cs/utils'],
+  transpilePackages: ['packages/ui', 'packages/utils'],
 };
 
 export default nextConfig;
