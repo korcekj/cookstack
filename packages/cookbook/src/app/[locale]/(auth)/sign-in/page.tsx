c92b54bd0f3cf9db76a31e137abf1d96e231a805
Ján Korček <korcekdevelopment@gmail.com>
2024-08-23 14:55:45 +0200

    feat: add empty page

---
+++ packages/cookbook/src/app/[locale]/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page = () => {
+  return <main>Sign in</main>;
+};
+
+export default Page;
---
+++ packages/cookbook/src/app/[locale]/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page = () => {
+  return <main>Sign up</main>;
+};
+
+export default Page;
