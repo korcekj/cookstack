168e6f282634073dd576f7052cfc57993214379b
Ján Korček <korcekdevelopment@gmail.com>
2024-08-23 14:55:17 +0200

    fix: match default locale

--- packages/cookbook/src/middleware.ts
+++ packages/cookbook/src/middleware.ts
@@ -8,5 +8,11 @@ export default createMiddleware({
 });
 
 export const config = {
-  matcher: ['/', `/(sk|en)/:path*`],
+  // https://next-intl-docs.vercel.app/docs/routing/middleware#matcher-no-prefix
+  matcher: [
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/_next` or `/_vercel`
+    // - … the ones containing a dot (e.g. `favicon.ico`)
+    '/((?!api|_next|_vercel|.*\\..*).*)',
+  ],
 };
