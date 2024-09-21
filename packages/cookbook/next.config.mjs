56d8186e5e9aac2a25c6d8995d9c8d90941d68bf
Ján Korček <korcekdevelopment@gmail.com>
2024-09-21 13:37:15 +0200

    refactor: replace built-in image component

--- packages/cookbook/next.config.mjs
+++ packages/cookbook/next.config.mjs
@@ -5,10 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@cs/ui', '@cs/utils'],
-  images: {
-    loader: 'custom',
-    loaderFile: './src/image-loader.js',
-  },
 };
 
 export default withNextIntl(nextConfig);
--- packages/cookbook/src/image-loader.js
+++
@@ -1,7 +0,0 @@
-export default function loader({ src, width, quality }) {
-  const url = new URL(src);
-  url.searchParams.set('w', width.toString());
-  url.searchParams.set('q', (quality || 'auto').toString());
-  url.searchParams.set('f', 'auto');
-  return url.toString();
-}
