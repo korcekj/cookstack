602b43fd6bda80828bc16a382f3550f383d50963
Ján Korček <korcekdevelopment@gmail.com>
2024-11-02 21:46:33 +0100

    feat: change runtime

--- packages/cookbook/src/app/[locale]/api/revalidate-user/route.ts
+++ packages/cookbook/src/app/[locale]/api/revalidate-user/route.ts
@@ -2,6 +2,8 @@ import { redirect } from '@/i18n/routing';
 import { revalidateTag } from 'next/cache';
 import { REDIRECTS } from '@/lib/constants';
 
+export const runtime = 'edge';
+
 export const GET = async () => {
   console.log('Revalidating user', Date.now());
   revalidateTag('user');
