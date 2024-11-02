f9fa58c243b1fda145776d8b2279c93494f9f00b
Ján Korček <korcekdevelopment@gmail.com>
2024-11-02 14:17:27 +0100

    refactor: remove cookie parsing

--- packages/cookbook/src/app/[locale]/api/revalidate-user/route.ts
+++ packages/cookbook/src/app/[locale]/api/revalidate-user/route.ts
@@ -1,10 +1,10 @@
 import { redirect } from '@/i18n/routing';
 import { revalidateTag } from 'next/cache';
 import { REDIRECTS } from '@/lib/constants';
-import { setResponseCookies } from '@/utils/cookies';
 
-export const GET = async (request: Request) => {
-  setResponseCookies(request.headers);
+export const dynamic = 'force-dynamic';
+
+export const GET = async () => {
   revalidateTag('user');
   redirect(REDIRECTS.home);
 };
