a69d4332417a16906660dac57940e90033eae545
Ján Korček <korcekdevelopment@gmail.com>
2024-11-16 22:13:34 +0100

    test: add auth test

--- packages/server/test/auth.test.ts
+++ packages/server/test/auth.test.ts
@@ -1,13 +1,17 @@
 import app from '../src/index';
 import { env } from 'cloudflare:test';
 
+let userId: string | null = null;
+let cookie: string | null = null;
+
 describe('Auth module', () => {
-  it('Should register a user - POST /api/auth/sign-up', async () => {
+  it('Should register a user - POST /api/auth/sign-up', async ({ headers }) => {
     const res = await app.request(
       '/api/auth/sign-up',
       {
         method: 'POST',
         headers: {
+          ...headers,
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
@@ -21,20 +25,26 @@ describe('Auth module', () => {
       env,
     );
 
+    const json = await res.json<{ user: { id: string } }>();
     expect(res.status).toBe(201);
-    expect(await res.json()).toMatchObject({
+    expect(json).toMatchObject({
       user: {
         email: 'john.doe@example.com',
       },
     });
+
+    userId = json.user.id;
   });
 
-  it('Should not register a user - POST /api/auth/sign-up', async () => {
+  it('Should not register a user - POST /api/auth/sign-up', async ({
+    headers,
+  }) => {
     const res = await app.request(
       '/api/auth/sign-up',
       {
         method: 'POST',
         headers: {
+          ...headers,
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
@@ -54,12 +64,15 @@ describe('Auth module', () => {
     });
   });
 
-  it('Should not login a user - POST /api/auth/sign-in', async () => {
+  it('Should not login a user - POST /api/auth/sign-in', async ({
+    headers,
+  }) => {
     const res = await app.request(
       '/api/auth/sign-in',
       {
         method: 'POST',
         headers: {
+          ...headers,
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
@@ -76,12 +89,13 @@ describe('Auth module', () => {
     });
   });
 
-  it('Should login a user - POST /api/auth/sign-in', async () => {
+  it('Should login a user - POST /api/auth/sign-in', async ({ headers }) => {
     const res = await app.request(
       '/api/auth/sign-in',
       {
         method: 'POST',
         headers: {
+          ...headers,
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
@@ -98,5 +112,29 @@ describe('Auth module', () => {
         email: 'john.doe@example.com',
       },
     });
+
+    cookie = res.headers.get('set-cookie');
+  });
+
+  it('Should logout a user - POST /api/auth/sign-out', async ({ headers }) => {
+    const res = await app.request(
+      '/api/auth/sign-out',
+      {
+        method: 'POST',
+        headers: {
+          ...headers,
+          Cookie: cookie ?? '',
+        },
+      },
+      env,
+    );
+
+    expect(res.status).toBe(200);
+    expect(await res.json()).toMatchObject({
+      user: null,
+    });
+
+    userId = null;
+    cookie = null;
   });
 });
