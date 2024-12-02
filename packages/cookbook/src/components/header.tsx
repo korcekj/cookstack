ac8198aac08f52c940c7ff3e055086f36ec385f2
Ján Korček <korcekdevelopment@gmail.com>
2024-12-02 13:55:24 +0100

    fix: display header on top

--- packages/cookbook/src/components/header.tsx
+++ packages/cookbook/src/components/header.tsx
@@ -17,7 +17,7 @@ import { SignOut } from '@/features/users/components/sign-out';
 
 export const Header: React.FC = () => {
   return (
-    <header className="bg-background sticky top-0 flex h-16 items-center gap-4 border-b px-4 md:px-6">
+    <header className="bg-background sticky top-0 z-10 flex h-16 items-center gap-4 border-b px-4 md:px-6">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
           href="/"
