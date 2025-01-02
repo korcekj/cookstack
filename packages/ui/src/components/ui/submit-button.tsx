d44f484a0ac7ba4826c926099940cdfe6339486e
Ján Korček <korcekdevelopment@gmail.com>
2025-01-02 14:43:42 +0100

    feat: prepend icon

--- packages/ui/src/components/ui/submit-button.tsx
+++ packages/ui/src/components/ui/submit-button.tsx
@@ -8,13 +8,17 @@ import { useFormStatus } from 'react-dom';
 import { Loader2 } from 'lucide-react';
 import { Button } from './button';
 
-type Props = ButtonProps & React.PropsWithChildren;
+type Props = ButtonProps &
+  React.PropsWithChildren & {
+    icon?: React.ReactNode;
+  };
 
-export const SubmitButton: React.FC<Props> = ({ children, ...rest }) => {
+export const SubmitButton: React.FC<Props> = ({ icon, children, ...rest }) => {
   const { pending } = useFormStatus();
 
   return (
     <Button type="submit" disabled={pending} {...rest}>
+      {!pending && icon}
       {pending && <Loader2 className="animate-spin" />}
       {children}
     </Button>
